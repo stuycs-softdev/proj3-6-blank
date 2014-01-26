d26b3030388541262ed023302bf6e819529e7f23
EliCohen96 <elic90@gmail.com>
2014-01-26 14:52:42 -0500

    added death predictions

--- movie_info.py
+++ movie_info.py
@@ -1,11 +1,16 @@
 import urllib
+import time
+
 from bs4 import BeautifulSoup
 from operator import itemgetter
+
+LIFEDATA = [78, 77, 76, 75, 74, 73, 72, 71, 70, 69, 68, 67, 66, 65, 65, 64, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50, 49, 48, 47, 46, 45, 44, 43, 43, 42, 41, 40, 39, 38, 37, 36, 35, 34, 33, 32, 32, 31, 30, 29, 28, 27, 26, 26, 25, 24, 23, 22, 21, 21, 20, 19, 18, 18, 17, 16, 15, 15, 14, 13, 13, 12, 11, 11, 10, 10, 9, 8, 8, 7, 7, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0]
+
 
 #I plan to revise this so you can get multiple results and choose your movie from them
 def findMovieLink(title):
 
-    formated = title.replace(' ','+');
+    formated = title.replace(' ','+')
 
     url = "http://www.imdb.com/find?q="+formated+"&s=all"
     page = BeautifulSoup(urllib.urlopen(url))
@@ -38,6 +43,12 @@ def getResults(actorLinks):
     unkownCount = 0
     totalCount = 0
 
+    deathlink = "http://www.ssa.gov/OACT/STATS/table4c6.html"
+    deathpage = BeautifulSoup(urllib.urlopen(deathlink))
+
+    allDead = 0
+    currentYear = int(time.strftime("%Y"))
+
     for x in actorLinks:
         info = {}
         info['name']= x[0]
@@ -48,24 +59,34 @@ def getResults(actorLinks):
             birth = page.find("time", {"itemprop" : "birthDate"}).find_all("a")[1].get_text()
             info['birth']= birth
         
+
             try:
                 death =  page.find("time", {"itemprop" : "deathDate"}).find_all("a")[1].get_text()
-                        
+                
                 info['death'] = death
                 info['status'] = "dead"
+                info['deathPrediction'] = death
                 deadCount = deadCount + 1
                 totalCount = totalCount + 1
-
+                allDead = death
+                
             except:
                 info['death'] = "alive"
                 info['status'] = "alive"
+                age = int(currentYear) - int(birth)
+                yearsLeft = LIFEDATA[age]
+                deathYear = currentYear + yearsLeft
+                info['deathPrediction'] = deathYear
                 aliveCount = aliveCount + 1
                 totalCount = totalCount + 1
 
+        
+        
         except:
             info['birth']= "unkown"
             info['death']= "unkown"
             info['status']= "unkown"
+            info['deathPrediction'] = 0
             unkownCount = unkownCount + 1
             totalCount = totalCount + 1
 
@@ -93,5 +114,5 @@ def movieInfo(title,maxActors):
     return data
 
 if __name__ == "__main__":
-   print movieInfo("fight club", 2)
-   
+    print(movieInfo("fight club", 10))
+    
